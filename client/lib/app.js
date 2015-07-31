e8c55d70778956f11572e42d65c5084946169a40
benjamin tillett <benjamin.tillett@gmail.com>
2015-07-31 11:50:12 +0100

    complete basic layout

--- client/lib/app.js
+++ client/lib/app.js
@@ -2,13 +2,17 @@ function Config($urlRouterProvider, $stateProvider, $locationProvider){
 	$locationProvider.html5Mode(true);
 
 	$stateProvider
-		.state('/',{
-			url: '/',
+		.state('home',{
+			url: '/home',
 			templateUrl: "client/index.ng.html",
 			controller: "IndexCtrl as index"
 		})
-
-	$urlRouterProvider.otherwise('/');
+		.state('home.dashboard',{
+			url: '/dashboard',
+			templateUrl: "client/dashboard.ng.html",
+			controller: "IndexCtrl as index"
+		})
+	$urlRouterProvider.otherwise('/home');
 }
 
 function IndexCtrl($mdSidenav){
@@ -19,8 +23,50 @@ function IndexCtrl($mdSidenav){
 	};
 }
 
+function SidenavCtrl() {
+	this.menu = [ 
+		{ title: "Dashboard", icon: "action:ic_dashboard_24px" },
+		{ title: "Freinds", icon: "social:ic_people_24px" },
+		{ title: "Chats", icon: "communication:ic_message_24px" }	
+	];
+
+	this.management = [
+		{ title: "Settings", icon: "action:ic_settings_24px" },
+		{ title: "Trash", icon: "action:ic_delete_24px" }
+	];
+}
+
+var themeIcons = function ($mdIconProvider) {
+
+  $mdIconProvider
+    .iconSet("social",
+             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
+
+    .iconSet("action",
+             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
+
+    .iconSet("communication",
+             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
+
+    .iconSet("content",
+             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
+
+    .iconSet("toggle",
+             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
+
+    .iconSet("navigation",
+             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
+
+    .iconSet("image",
+             "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
+
+};
+
+
 if (Meteor.isClient){
 	angular.module('app',['angular-meteor','ui.router','ngMaterial'])
 		.config(['$urlRouterProvider', '$stateProvider', '$locationProvider',Config])
-		.controller('IndexCtrl',['$mdSidenav',IndexCtrl]);
+		.controller('IndexCtrl',['$mdSidenav',IndexCtrl])
+		.controller('SidenavCtrl',['$mdSidenav',SidenavCtrl])
+		.config(themeIcons);
  }
